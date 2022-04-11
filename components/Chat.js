ed11fbd1c6682345d2c9ca75943e4b634e2ecb0e
Mike H <72580577+mekanik20@users.noreply.github.com>
2022-04-11 19:49:11 -0400

    trying to fix user text issue

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,59 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
-  render() {
-    let { bgColor } = this.props.route.params;
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'You are connected to the chat',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    }
+  }
+
+  componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+  }
+
+  onSend(messages = []) {
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }))
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: 'white'
+          }
+        }}
+      />
+    )
+  }
+
+  render() {
+    let { bgColor } = this.props.route.params;
 
     return (
       <View
@@ -14,7 +62,18 @@ export default class Chat extends React.Component {
           flex: 1,
         }}
       >
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
 }
+
+
